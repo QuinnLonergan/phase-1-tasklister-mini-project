1f3c080dd2f6e558e7c1c765dd0f1615951b3a23
Quinn Lonergan <qui99lo@gmail.com>
2021-07-06 12:21:53 -0400

    Completed deliverables

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#create-task-form").addEventListener('submit', (e) => {
     e.preventDefault()
     let li = document.createElement('li')
-    li.textContent = e.target.new-task-description.value
+    li.textContent = e.target["new-task-description"].value
     document.querySelector('#tasks').append(li)
   })
   console.log(e.target.new-task-description.value)
